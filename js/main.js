64f92a319ac2ac691a85c16a2b309d86c85ed818
chriz-3656 <chriz-3656@users.noreply.github.com>
2026-09-20 13:41:33 -0400

    chore: Update fallback download URL to v1.4.1

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 // Configuration Constants
 const CONFIG = {
-  DOWNLOAD_URL: "https://github.com/chriz-3656/Pulse-Music/releases/download/v1.4.0/PulseMusic-v1.4.0.apk",
+  DOWNLOAD_URL: "https://github.com/chriz-3656/Pulse-Music/releases/download/v1.4.1/PulseMusic-v1.4.1.apk",
   GITHUB_URL: "https://github.com/chriz-3656/Pulse-Music"
 };
 
